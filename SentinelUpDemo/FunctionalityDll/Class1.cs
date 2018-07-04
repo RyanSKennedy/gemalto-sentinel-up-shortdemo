ed9164af04889ab347f333a4b66786a90b3cc286
RyanSK(Work) <ryan_s.kennedy@mail.ru>
2018-07-04 13:25:01 +0300

    Add Sample

---
+++ SentinelUpDemo/FunctionalityDll/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalityDll
+{
+    public class Class1
+    {
+        public static string version = "v2";
+
+        public string SayHi()
+        {
+            return "Hi Men!";
+        }
+
+        public string SayBay()
+        {
+            return "Bay Men!";
+        }
+
+        public bool EnableButton()
+        {
+            if (version == "v2") {
+                return true;
+            } else if (version == "v1") {
+                return false;
+            } else {
+                return false;
+            }
+        }
+    }
+}
---
+++ SentinelUpDemo/SentinelUpDemo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FunctionalityDll;
+
+namespace SentinelUpDemo
+{
+    public partial class Form1 : Form
+    {
+        FunctionalityDll.Class1 newFunction = new Class1();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(newFunction.SayHi());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(newFunction.SayBay());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + Class1.version;
+            button2.Visible = newFunction.EnableButton();
+        }
+    }
+}
